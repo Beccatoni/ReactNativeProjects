2e75d69a9bd67998d4054922d65b177946f29608
Rebecca MUTONI <82598316+Beccatoni@users.noreply.github.com>
2024-02-19 13:36:56 +0100

    my apps

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, ScrollView, Text, View, StyleSheet } from "react-native";
+import Signin from "./screen/SignIn";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    {/* <SafeAreaView style={{backgroundColor: "red"}}>
+      <ScrollView>
+        <Text>GFHJKL;'</Text>
+      </ScrollView>
+    </SafeAreaView> */}
+
+    <View>
+      <Signin/>
     </View>
+
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFA586',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
+export default App;
---
+++ styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor:'#FFA586',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '30%'
+    },
+    buttonRequest: {
+        backgroundColor:'#384358'
+    }
+});
+
+
+module.exports = {
+    style
+}
